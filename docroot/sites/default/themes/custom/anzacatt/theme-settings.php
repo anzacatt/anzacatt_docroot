1e6596031bcad4187f469515cb6676259633bfaf
Ivan Grynenko <ivan@salsadigital.com.au>
2017-01-17 10:48:45 +1100

    [ANZACATT-34] Refactored govcms_ui_kit references in theme settings to use anzacatt name.

--- docroot/sites/default/themes/custom/anzacatt/theme-settings.php
+++ docroot/sites/default/themes/custom/anzacatt/theme-settings.php
@@ -9,7 +9,7 @@
  * Implements hook_system_theme_settings_alter().
  */
 function anzacatt_form_system_theme_settings_alter(&$form, $form_state) {
-  $form['govcms_ui_kit_options'] = array(
+  $form['anzacatt_options'] = array(
     '#type' => 'fieldset',
     '#title' => t('govCMS UI Kit settings'),
     '#weight' => 5,
@@ -17,31 +17,31 @@ function anzacatt_form_system_theme_settings_alter(&$form, $form_state) {
     '#collapsed' => FALSE,
   );
 
-  $form['govcms_ui_kit_options']['govcms_ui_kit_header_title'] = array(
+  $form['anzacatt_options']['anzacatt_header_title'] = array(
     '#type'          => 'textfield',
     '#title'         => t('Header title'),
-    '#default_value' => theme_get_setting('govcms_ui_kit_header_title'),
+    '#default_value' => theme_get_setting('anzacatt_header_title'),
     '#description'   => t("Text to display beside the site logo in the top header."),
   );
 
-  $form['govcms_ui_kit_options']['govcms_ui_kit_header_logo_alt'] = array(
+  $form['anzacatt_options']['anzacatt_header_logo_alt'] = array(
     '#type'          => 'textfield',
     '#title'         => t('Header logo alternative text'),
-    '#default_value' => theme_get_setting('govcms_ui_kit_header_logo_alt'),
+    '#default_value' => theme_get_setting('anzacatt_header_logo_alt'),
     '#description'   => t("Alternative text to assign to the logo in the top header."),
   );
 
-  $form['govcms_ui_kit_options']['govcms_ui_kit_footer_copyright'] = array(
+  $form['anzacatt_options']['anzacatt_footer_copyright'] = array(
     '#type'          => 'textfield',
     '#title'         => t('Footer copyright'),
-    '#default_value' => theme_get_setting('govcms_ui_kit_footer_copyright'),
+    '#default_value' => theme_get_setting('anzacatt_footer_copyright'),
     '#description'   => t("Text to display beside the sub menu links. Defaults to <em>&copy; [current year]. [Site Name]. All rights reserved.</em>"),
   );
 
-  $form['govcms_ui_kit_options']['govcms_ui_kit_override_image_styles'] = array(
+  $form['anzacatt_options']['anzacatt_override_image_styles'] = array(
     '#type'          => 'checkbox',
     '#title'         => t('Override image styles'),
-    '#default_value' => theme_get_setting('govcms_ui_kit_override_image_styles'),
+    '#default_value' => theme_get_setting('anzacatt_override_image_styles'),
     '#description'   => t("Enable this to override any user-defined image styles with govCMS UI Kit default styles. Disabling this is recommend if modifying site."),
   );
 }
