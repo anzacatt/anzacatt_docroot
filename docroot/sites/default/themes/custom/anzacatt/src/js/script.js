14e40d55a47d5c65dc56b09b2d6e6b0893864739
Ivan Grynenko <ivan@salsadigital.com.au>
2017-01-18 17:29:43 +1100

    [ANZACATT-14] Added loader PHP and JS to apply background images to the top header.

--- docroot/sites/default/themes/custom/anzacatt/src/js/script.js
+++ docroot/sites/default/themes/custom/anzacatt/src/js/script.js
@@ -13,11 +13,34 @@ var desktop_column = 1170;
  */
 (function($, Drupal, window, document, undefined) {
 
+  /**
+   * Picks a random element of an array of anything.
+   *
+   * @param arr
+   *   Array for processing.
+   * @returns mixed
+   *   A random array element.
+   */
+  function randomFrom(arr) {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
   Drupal.behaviors.anzacatt = {
     attach: function(context, settings) {
       // Object Fit Polyfill for IE. Used on News Teaser Images.
       objectFitImages();
     }
   };
+
+  Drupal.behaviors.anzacatt_parliament_images = {
+    attach: function(context, settings) {
+      // Add random parliament images as header background.
+      var $images = Drupal.settings.anzacatt.parliament_images;
+      var selectedImage = randomFrom($images);
+      var element = $('#page .content-header');
+      element.attr('style', 'background-image: url(' + selectedImage + ')');
+    }
+  }
 
 })(jQuery, Drupal, this, this.document);
