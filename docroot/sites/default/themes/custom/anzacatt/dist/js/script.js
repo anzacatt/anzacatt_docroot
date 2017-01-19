f5a506cab68171f0a09f1c6c80ad6d7efc67a186
Ivan Grynenko <ivan@salsadigital.com.au>
2017-01-19 11:57:14 +1100

    [ANZACATT-14] Cleaned up JS files from duplicate functions.

--- docroot/sites/default/themes/custom/anzacatt/dist/js/script.js
+++ docroot/sites/default/themes/custom/anzacatt/dist/js/script.js
@@ -142,53 +142,6 @@ var desktop_column = 1170;
 })(jQuery, Drupal, this, this.document);
 
 /**
- * Global variables.
- */
-var desktop_breakpoint = 1200;
-var large_tablet_breakpoint = 1024;
-var tablet_breakpoint = 768;
-var mobile_breakpoint = 420;
-
-var desktop_column = 1170;
-
-/**
- * govCMS general bootstrapping.
- */
-(function($, Drupal, window, document, undefined) {
-
-  /**
-   * Picks a random element of an array of anything.
-   *
-   * @param arr
-   *   Array for processing.
-   * @returns mixed
-   *   A random array element.
-   */
-  function randomFrom(arr) {
-    var randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-  }
-
-  Drupal.behaviors.anzacatt = {
-    attach: function(context, settings) {
-      // Object Fit Polyfill for IE. Used on News Teaser Images.
-      objectFitImages();
-    }
-  };
-
-  Drupal.behaviors.anzacatt_parliament_images = {
-    attach: function(context, settings) {
-      // Add random parliament images as header background.
-      var $images = Drupal.settings.anzacatt.parliament_images;
-      var selectedImage = randomFrom($images);
-      var element = $('#page .content-header');
-      element.attr('style', 'background-image: url(' + selectedImage + ')');
-    }
-  }
-
-})(jQuery, Drupal, this, this.document);
-
-/**
  * Header Search Field.
  */
 (function($, Drupal, window, document, undefined) {
