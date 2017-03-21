df5a132981c225d56c3358905e2997b30c3041a7
Gregory Netsas <gregory@userfriendly.tech>
2017-03-21 16:10:49 +1100

    Update template.php
    - Changed `und` to use the node language instead.
    - Added the logic of appending the post-nominals to the node title.

--- docroot/sites/default/themes/custom/anzacatt_admin/template.php
+++ docroot/sites/default/themes/custom/anzacatt_admin/template.php
@@ -42,7 +42,7 @@ function anzacatt_admin_pathauto_pattern_alter(&$alias, $context) {
 /**
  * Implements hook_form_alter().
  */
-function anzacatt_admin_form_alter(&$form, &$form_state) {
+function anzacatt_admin_form_alter(&$form, &$form_state, $form_id) {
   if (!empty($form['#node_edit_form'])) {
     $form['#submit'][] = 'anzacatt_admin_node_form_submit';
   }
@@ -66,11 +66,12 @@ function anzacatt_admin_node_form_submit($form, &$form_state) {
     // membersonly. Update the URL alias to remove it.
     $form_state['values']['path']['pathauto'] = 1;
   }
-
   if ($form_state['values']['type'] === 'member_profile') {
-    $name_title = $form_state['values']['field_name_title']['und']['0']['tid'];
-    $first_name = $form_state['values']['field_first_name']['und']['0']['value'];
-    $last_name = $form_state['values']['field_last_name']['und']['0']['value'];
+    $lang = $form_state['node']->language;
+    $name_title = $form_state['values']['field_name_title'][$lang]['0']['tid'];
+    $first_name = $form_state['values']['field_first_name'][$lang]['0']['value'];
+    $last_name = $form_state['values']['field_last_name'][$lang]['0']['value'];
+    $post_nominals = $form_state['values']['field_post_nominals'][$lang]['0']['value'];
     // The node title field for Member Profiles is hidden from the form.
     // Construct it by concatenating the first_name and last_name fields.
     $full_name =  $first_name . " " . $last_name;
@@ -79,6 +80,10 @@ function anzacatt_admin_node_form_submit($form, &$form_state) {
       $term = taxonomy_term_load($name_title);
       $name_title = $term->name;
       $full_name = $name_title . " " . $full_name;
+    }
+    if ($post_nominals) {
+      // Append post_nominals if there are any (optional field).
+      $full_name = $full_name . " " . $post_nominals;
     }
     $form_state['values']['title'] = $full_name;
   }
